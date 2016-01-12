4eb52e5e7b7c8492cc2353ab97437ce90feca1be
peaseloxes <peaseloxes@gmail.com>
2016-01-12 04:26:04 -0600

    Told you it was buggy.

--- Domain/src/main/java/entities/rest/CustomerProduct.java
+++ Domain/src/main/java/entities/rest/CustomerProduct.java
@@ -1,5 +1,6 @@
 package entities.rest;
 
+import entities.Product;
 import entities.abs.PersistenceEntity;
 import javax.persistence.Entity;
 import lombok.*;
@@ -21,10 +22,30 @@ public class CustomerProduct extends PersistenceEntity {
     private int amount;
     private double price;
 
+    /**
+     * CustomerProduct constructor.
+     *
+     * @param id     the product id.
+     * @param name   the product name.
+     * @param price  the product price.
+     * @param amount the amount in stock.
+     */
     public CustomerProduct(final String id, final String name, final int amount, final double price) {
         this.setId(id);
         this.name = name;
         this.amount = amount;
         this.price = price;
+    }
+
+    /**
+     * CustomerProduct constructor.
+     *
+     * @param product the product to extract data from.
+     */
+    public CustomerProduct(final Product product) {
+        this.setId(product.getId());
+        this.name = product.getName();
+        this.amount = product.getStock();
+        this.price = product.getPrice();
     }
 }
